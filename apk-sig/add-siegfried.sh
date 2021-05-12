17ae8b084e7cebf76f9d400995d405c0d9224567
bitsgalore <johan.vanderknijff@kb.nl>
2021-05-12 17:15:50 +0200

    added Siegfried build script

---
+++ apk-sig/add-siegfried.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script creates Siegfried signatures from the customs
+# DROID signatures in this directory. After build they will
+# replace the default signatures! To revert to the original
+# signatures afterwards, manually run "sudo roy build"
+
+sudo cp *.xml /usr/share/siegfried/
+sudo roy build -noreports \
+     -droid /usr/share/siegfried/apk-standard-base.xml \
+     -container /usr/share/siegfried/apk-container-base.xml
